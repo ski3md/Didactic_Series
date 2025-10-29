cd0c8f893ddc75efed97d8a94b497f468d648cb7
ski3md <askiatablet@gmail.com>
2025-10-28 22:58:56 -0500

    Refactor tailwind.config.js structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [/** @type {import('tailwindcss').Config} */
-module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
- ],
   theme: {
     extend: {
       fontFamily: {
@@ -19,18 +11,18 @@ module.exports = {
         serif: ['Lora', 'serif'],
       },
       colors: {
-        'primary': {
-          '50': '#eef2ff',
-          '100': '#e0e7ff',
-          '200': '#c7d2fe',
-          '300': '#a5b4fc',
-          '400': '#818cf8',
-          '500': '#6366f1',
-          '600': '#4f46e5',
-          '700': '#4338ca',
-          '800': '#3730a3',
-          '900': '#312e81',
-          '950': '#1e1b4b',
+        primary: {
+          50: '#eef2ff',
+          100: '#e0e7ff',
+          200: '#c7d2fe',
+          300: '#a5b4fc',
+          400: '#818cf8',
+          500: '#6366f1',
+          600: '#4f46e5',
+          700: '#4338ca',
+          800: '#3730a3',
+          900: '#312e81',
+          950: '#1e1b4b',
         },
       },
       animation: {
